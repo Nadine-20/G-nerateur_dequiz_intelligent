8c0adcc6ae4d81369caa76446536de12db0db78e
MahdiLcoder <labidimahdi95@gmail.com>
2025-06-30 18:49:28 +0100

    database connection

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_pymongo import PyMongo
+import os
+from dotenv import load_dotenv
 
+load_dotenv() 
 app = Flask(__name__)
 
-@app.route("/")
-def helloworld():
-    return "Hello, World!"
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+
+mongo = PyMongo(app)
+db = mongo.db  
+
+try:
+    mongo.cx.admin.command('ping')  
+    print("✅ MongoDB connected successfully.")
+except Exception as e:
+    print("❌ MongoDB connection failed:", e)
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
